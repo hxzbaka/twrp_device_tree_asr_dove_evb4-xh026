310a0d3d671207c858a8684c204829cddedda3cc
hxzbaka <165454150+hxzbaka@users.noreply.github.com>
2025-08-31 19:46:51 +0800

    Set watch ui🤩

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,7 +108,7 @@ PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
 TW_DEFAULT_LANGUAGE := zh_CN
-TW_THEME := portrait_hdpi
+TW_THEME := watch_mdpi
 TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
